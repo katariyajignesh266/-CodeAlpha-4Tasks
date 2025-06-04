db4afc2492951df4226acd2646840dcdcdf1b71d
Katariya jignesh <katariyajignesh266@gmail.com>
2025-06-04 18:04:21 +0530

    Create eda-titanic.py

---
+++ TASK 2 EDA/eda-titanic.py
@@ -0,0 +1,30 @@
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("titanic.csv")
+
+print("First 5 rows:")
+print(df.head())
+
+print("\nDataset Info:")
+print(df.info())
+
+print("\nMissing values:")
+print(df.isnull().sum())
+
+print("\nStatistics:")
+print(df.describe())
+
+sns.countplot(x='Survived', data=df)
+plt.title("Survival Count")
+plt.show()
+
+sns.countplot(x='Survived', hue='Sex', data=df)
+plt.title("Survival by Gender")
+plt.show()
+
+sns.histplot(df['Age'].dropna(), bins=30, kde=True)
+plt.title("Age Distribution")
+plt.show()
